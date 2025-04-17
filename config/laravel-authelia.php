5629fcb579c6c3bc0159fe19dbae63357db12eff
Waris Agung Widodo <ido.alit@gmail.com>
2025-04-17 11:28:15 +0700

    update default configuration

--- config/laravel-authelia.php
+++ config/laravel-authelia.php
@@ -15,24 +15,26 @@
  * ----------	---	---------------------------------------------------------  *
  */
 
- return [
- 
-    'client_id' => env('OAUTH_CLIENT_ID', 'oauth-client-id'),
-    'client_secret' => env('OAUTH_CLIENT_SECRET', 'oauth-client-secret'),
-    'redirect_uri' => env('OAUTH_REDIRECT_URI', 'http://localhost:8000/oauth/callback'),
+return [
+    /*
+     * The default guard for the application
+     * This is used to determine which guard to use for authentication
+     */
+    'client_id' => env('OAUTH_CLIENT_ID', 'oauth_client_id'),
+    'client_secret' => env('OAUTH_CLIENT_SECRET', 'oauth_client_secret'),
+    'redirect_uri' => env('OAUTH_REDIRECT_URI', 'oauth/callback'),
 
     'scope' => env('OAUTH_SCOPE', 'email profile'),
     'response_type' => env('OAUTH_RESPONSE_TYPE', 'code'),
     'grant_type' => env('OAUTH_GRANT_TYPE', 'authorization_code'),
 
-    'base_url' => env('OAUTH_BASE_URL', 'http://localhost:8000/oauth'),
-    'authorization_url' => env('OAUTH_AUTHORIZATION_URL', 'http://localhost:8000/oauth/authorize'),
-    'token_url' => env('OAUTH_TOKEN_URL', 'http://localhost:8000/oauth/token'),
-    'user_info_url' => env('OAUTH_USER_INFO_URL', 'http://localhost:8000/oauth/userinfo'),
+    /**
+     * Configuration for the OAuth2 server
+     * The default values are for the Poltekkes Semarang OAuth2 server
+     */
+    'base_url' => env('OAUTH_BASE_URL', 'https://auth.poltekkes-smg.ac.id'),
+    'authorization_url' => env('OAUTH_AUTHORIZATION_URL', 'https://auth.poltekkes-smg.ac.id/api/oidc/authorization'),
+    'token_url' => env('OAUTH_TOKEN_URL', 'https://auth.poltekkes-smg.ac.id/api/oidc/token'),
+    'user_info_url' => env('OAUTH_USER_INFO_URL', 'https://auth.poltekkes-smg.ac.id/api/oidc/userinfo'),
     'user_info_email_field' => env('OAUTH_USER_INFO_EMAIL_FIELD', 'email'),
-
-    'admin_page' => env('OAUTH_ADMIN_PAGE', 'http://localhost:8000/admin'),
-    'admin_page_route' => env('OAUTH_ADMIN_PAGE_ROUTE', 'admin'),
-
-    'user_page_route' => env('OAUTH_USER_PAGE_ROUTE', 'app'),
 ];
